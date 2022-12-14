dc12433107f558f3530c63e1d9477bd366d34378
lokesh <lokeshnp77@gmail.com>
2022-12-14 13:11:30 +0530

    html-report & logging

---
+++ PyTestDemo/BaseClass.py
@@ -0,0 +1,20 @@
+import inspect
+import logging
+
+# Command for reporting logs in html report -> pytest --html=report.html --self-contained-html
+
+
+class BaseClass:
+    def getLogger(self):
+        loggerName = inspect.stack()[1][3]
+        logger = logging.getLogger(loggerName)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+
+        logger.addHandler(fileHandler)  # filehandler object
+
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+
--- PyTestDemo/test_fixtureData.py
+++ PyTestDemo/test_fixtureData.py
@@ -1,9 +1,13 @@
 import pytest
 
+from PyTestDemo.BaseClass import BaseClass
+
 
 @pytest.mark.usefixtures("dataLoad")
-class TestExample2:
+class TestExample2(BaseClass):
 
     def test_editProfile(self, dataLoad):
-        print(dataLoad[0])
+        log = self.getLogger()
+        log.info(dataLoad[0])
+        log.info(dataLoad[2])
         print(dataLoad[2])
